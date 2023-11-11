b68526c50d2150c5274c00c3ca667aadb6cf6375
HoangQuan13579 <150511728+HoangQuan13579@users.noreply.github.com>
2023-11-11 15:47:08 +0700

    Add files via upload
    Nhóm 20

---
+++ 44.py
@@ -0,0 +1,10 @@
+a=int(input("Date:"))
+b=int(input("Month:"))
+if a==1 and b==1:
+    print("New year's day")
+elif a==1 and b==7:
+    print("Canada day")
+elif a==25 and b==12:
+    print("Christmas day")
+else: 
+    print("Khong phai ngay le!!!")
